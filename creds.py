d891b5ae96bab077881d8c4c6d5aca83c86d1637
subashvfc <88789785+subashvfc@users.noreply.github.com>
2022-05-26 10:34:53 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,12 +5,12 @@
 
 
 class cred:
-    BOT_NAME = os.getenv("subtitlestranslater_bot")
-    BOT_TOKEN = os.getenv("5338762282:AAFQHsEu-qZVz65bRFKlqr4t3garDMNaFfs")  # From botfather
-    API_ID = os.getenv(
+    BOT_NAME = "subtitlestranslater_bot"
+    BOT_TOKEN = "5338762282:AAFQHsEu-qZVz65bRFKlqr4t3garDMNaFfs"  # From botfather
+    API_ID = 
         "17545386"
-    )  # "Get this value from my.telegram.org! Please do not steal"
-    API_HASH = os.getenv(
+      # "Get this value from my.telegram.org! Please do not steal"
+    API_HASH = 
         "f1d5ee6adccd34db0aab30115f4c30db"
-    )  # "Get this value from my.telegram.org! Please do not steal"
-    FIREBASE = os.getenv("https://subtrans-37fa1-default-rtdb.firebaseio.com/")  # From Firebase database
+      # "Get this value from my.telegram.org! Please do not steal"
+    FIREBASE = "https://subtrans-37fa1-default-rtdb.firebaseio.com/"  # From Firebase database
