c21d79e07a7818c3ef64feb69ee9843c0d820dd7
subashvfc <88789785+subashvfc@users.noreply.github.com>
2022-05-26 10:38:06 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -7,10 +7,10 @@
 class cred:
     BOT_NAME = "subtitlestranslater_bot"
     BOT_TOKEN = "5338762282:AAFQHsEu-qZVz65bRFKlqr4t3garDMNaFfs"  # From botfather
-    API_ID = 
-        "17545386"
+    API_ID = "17545386"
+        
       # "Get this value from my.telegram.org! Please do not steal"
-    API_HASH = 
-        "f1d5ee6adccd34db0aab30115f4c30db"
+    API_HASH = "f1d5ee6adccd34db0aab30115f4c30db"
+        
       # "Get this value from my.telegram.org! Please do not steal"
     FIREBASE = "https://subtrans-37fa1-default-rtdb.firebaseio.com/"  # From Firebase database
